191cd5da28f9b5a66fe982f7d8252c2ab0d7f6b2
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-05-26 00:00:55 +0600

    CRUD operations for social media with error handling and revalidation

---
+++ src/actions/SocialMedia/index.ts
@@ -0,0 +1,115 @@
+"use server";
+import { revalidateTag } from "next/cache";
+import { cookies } from "next/headers";
+
+export const createSocialMedia = async (
+  socialMediaData: FormData
+): Promise<any> => {
+  try {
+    const token = (await cookies()).get("accessToken")?.value;
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/social-media`,
+      {
+        method: "POST",
+        body: socialMediaData,
+        headers: {
+          Authorization: token || "",
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to update profile");
+    }
+    // revalidateTag("user-profile");
+    revalidateTag("user-profile");
+
+    return await res.json();
+  } catch (error: any) {
+    console.error("Update SocialMedia error:", error.message);
+    return { error: error.message };
+  }
+};
+
+export const getAllSocialMedias = async () => {
+  try {
+    const token = (await cookies()).get("accessToken")?.value;
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/social-media/get-all-my-social-medias`,
+      {
+        headers: {
+          Authorization: token || "",
+        },
+        next: {
+          tags: ["user-profile"],
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("You are not authorized!");
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error: any) {
+    console.error("Get Social Media error:", error.message);
+    return { error: error.message };
+  }
+};
+
+export const updateSocialMedia = async (socialMediaData: FormData) => {
+  try {
+    const token = (await cookies()).get("accessToken")?.value;
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/social-media`,
+      {
+        method: "PATCH",
+        body: socialMediaData,
+        headers: {
+          Authorization: token || "",
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to update skill");
+    }
+
+    revalidateTag("user-profile");
+
+    return await res.json();
+  } catch (error: any) {
+    console.error("Update skill error:", error.message);
+    return { error: error.message };
+  }
+};
+
+// Skill deleted
+export const deleteSocialMedia = async (socialMediaId: string) => {
+  try {
+    const token = (await cookies()).get("accessToken")?.value;
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/skill/${socialMediaId}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: token || "",
+        },
+      }
+    );
+    if (!res.ok) {
+      throw new Error("Failed to delete Social Media");
+    }
+
+    revalidateTag("user-profile");
+
+    return await res.json();
+  } catch (error: any) {
+    console.error("Delete Social Media error:", error.message);
+    return { error: error.message };
+  }
+};
