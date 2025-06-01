a121ed31493cd11f14f6e60ebbe68dacbc4aceed
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-06-02 00:22:57 +0600

    Update BASEURL to point to the correct production API endpoint

--- src/lib/URL/index.ts
+++ src/lib/URL/index.ts
@@ -1,3 +1,3 @@
 // export const BASEURL = "http://localhost:5000/api/v1";
-export const BASEURL = "https://managemehedimehad.vercel.app";
+export const BASEURL = "https://portfolio-server-one-mu.vercel.app/api/v1";
 export const FRONTENDURL = "https://managemehedimehad.vercel.app";
